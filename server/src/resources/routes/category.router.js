4bb47bad0668378bd9fb9ec2c4d2381265620aa5
Carro <carolinaisabellaandersson@gmail.com>
2024-01-02 16:11:24 +0100

    Implement authorization for product and category CRUD actions

--- server/src/resources/routes/category.router.js
+++ server/src/resources/routes/category.router.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const { getAllCategories, getCategory, createCategory } = require("../controllers/category.controller")
-
+const { adminAuth } = require('../middlewares')
 const categoryRouter = Router()
     .get('/categories', getAllCategories)
     .get('/categories/:id', getCategory)
-    .post('/categories', createCategory)
+    .post('/categories', adminAuth, createCategory)
 
 
 
--- server/src/resources/routes/product.router.js
+++ server/src/resources/routes/product.router.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const { getAllProducts, getProduct, getProductsByCategory, createProduct, updateProduct, deleteProduct } = require('../controllers/product.controller')
+const { adminAuth } = require('../middlewares')
 
 const productRouter = Router()
     .get('/products', getAllProducts)
     .get('/products/:id', getProduct)
     .get('/products/byCategory/:id', getProductsByCategory)
-    .post('/products', createProduct)
-    .put('/products/:id', updateProduct)
-    .delete('/products/:id', deleteProduct)
+    .post('/products', adminAuth, createProduct)
+    .put('/products/:id', adminAuth, updateProduct)
+    .delete('/products/:id', adminAuth, deleteProduct)
 
 
 module.exports = { productRouter }
