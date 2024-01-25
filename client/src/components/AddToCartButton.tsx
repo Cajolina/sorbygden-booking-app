033846317033287c4f3c3e2141e4a62d0ae18f55
Carro <carolinaisabellaandersson@gmail.com>
2024-01-25 20:52:18 +0100

    Fixed bug: Disable the button when the event is out of stock.

--- client/src/components/AddToCartButton.tsx
+++ client/src/components/AddToCartButton.tsx
@@ -1,26 +1,33 @@
 import { useCartContext } from "../context/CartContext";
-import { IEvent, IFacility, IProduct } from "../Interfaces";
+import { IEvent, IFacility } from "../Interfaces";
 import { Button } from "antd";
+
 type Props = {
-  product: IProduct;
+  product: IEvent | IFacility;
 };
 
 function AddToCartButton({ product }: Props) {
   const { addToCart, cart } = useCartContext();
-  const totalQuantityInCart = cart.reduce(
-    (total, item) => total + item.quantity,
-    0
-  );
+  //calculate the total quantity of items in the shopping cart by summing up the quantity of each item
+  // const totalQuantityInCart = cart.reduce(
+  //   (total, item) => total + item.quantity,
+  //   0
+  // );
+
+  // Find the cart item for the current product
+  const cartItem = cart.find((item) => item.product._id === product._id);
+
+  // Check if the product is an event or facility and update disableButtonCondition
   const disableButtonCondition =
-    totalQuantityInCart >= (product as IEvent).inStock ||
-    (product as IFacility).availability == false;
+    (product as IEvent).inStock === 0 ||
+    (product as IFacility).availability === false ||
+    (cartItem?.quantity || 0) >= (product as IEvent).inStock;
 
   return (
     <div>
       <Button
         onClick={(e) => {
           e.preventDefault();
-
           addToCart(product);
         }}
         disabled={disableButtonCondition}
